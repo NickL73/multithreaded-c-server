200a48982d21bcbdb4982eeeda26a5b7c53bf742
Nicholas Lunsford <nick.lunsford73@gmail.com>
2025-07-11 21:04:21 -0400

    Sets up signal handlers for eventual main loop

---
+++ src/debug_print.h
@@ -0,0 +1,63 @@
+/**
+ * @file debug_print.h
+ * @author nick
+ * @date 7/11/25
+ * @brief
+ */
+#ifndef DEBUG_PRINT_H
+#define DEBUG_PRINT_H
+
+#ifdef NDEBUG
+#define LOG_MSG(level, ...) ((void)0)
+#else
+
+#include <stdio.h>
+#include <string.h>
+
+typedef enum
+{
+    LOG_DEBUG,
+    LOG_INFO,
+    LOG_WARN,
+    LOG_ERROR,
+    LOG_FATAL
+} log_level_t;
+
+// Define the color codes associated with each log level
+#define COLOR_RESET  "\x1b[0m"  // reset the terminal color
+#define COLOR_DEBUG  "\x1b[37m" // white
+#define COLOR_INFO   "\x1b[32m" // green
+#define COLOR_WARN   "\x1b[33m" // yellow
+#define COLOR_ERROR  "\x1b[31m" // red
+#define COLOR_FATAL  "\x1b[31m" // also red
+
+// Don't want the full filepath, as it can clutter the output so just get relevant info
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+// Set a default maximum logging level if one wasn't defined at compilation
+#ifndef LOG_LEVEL
+#define LOG_LEVEL LOG_DEBUG
+#endif // LOG_LEVEL
+
+#define LOG_MSG(level, ...)                                                                                  \
+    do                                                                                                       \
+    {                                                                                                        \
+        if (LOG_LEVEL <= level)                                                                              \
+        {                                                                                                    \
+            const char * p_level_colors[] = {COLOR_DEBUG, COLOR_INFO, COLOR_WARN, COLOR_ERROR, COLOR_FATAL}; \
+            printf("[%s%s%s][%s:%d] ", p_level_colors[level], #level, COLOR_RESET, __FILENAME__, __LINE__);  \
+            printf(__VA_ARGS__);                                                                             \
+            printf("\n");                                                                                    \
+        }                                                                                                    \
+    } while (0)
+
+#endif // NDEBUG
+
+// Define some easier macros to use instead of just the general macro
+#define LOG_DEBUG(...) LOG_MSG(LOG_DEBUG, __VA_ARGS__)
+#define LOG_INFO(...)  LOG_MSG(LOG_INFO, __VA_ARGS__)
+#define LOG_WARN(...)  LOG_MSG(LOG_WARN, __VA_ARGS__)
+#define LOG_ERROR(...) LOG_MSG(LOG_ERROR, __VA_ARGS__)
+#define LOG_FATAL(...) LOG_MSG(LOG_FATAL, __VA_ARGS__)
+
+#endif // DEBUG_PRINT_H
--- src/main.c
+++ src/main.c
@@ -1,4 +1,50 @@
-int main(void)
+/**
+ * @file main.c
+ * @author nick
+ * @date 7/11/25
+ * @brief
+ */
+#include "debug_print.h"
+
+#include <signal.h>
+
+
+/* GLOBAL DEFINITIONS AND VALUES */
+#define SERVER_PORT 1337
+volatile sig_atomic_t should_shutdown = 0;
+
+/* STATIC FUNCTION DECLARATIONS */
+static void sighandler(int signum);
+
+int main(int argc, char * argv[])
 {
+    int err = 0;
+
+    /* Setup the signal handler to attempt a graceful shutdown on SIGINT and SIGTERM */
+    struct sigaction sa = {0};
+    sa.sa_handler       = sighandler;
+    sa.sa_flags         = 0;
+    if (0 != sigemptyset(&sa.sa_mask))
+    {
+        LOG_FATAL("Failed to empty server's signal set");
+        goto end;
+    }
+
+    if ((0 != sigaction(SIGINT, &sa, NULL)) || (0 != sigaction(SIGTERM, &sa, NULL)))
+    {
+        LOG_FATAL("Failed to set server's signal handler");
+        goto end;
+    }
+
+    /* Don't let SIGPIPE break the server either, just ignore it */
+    (void)signal(SIGPIPE, SIG_IGN);
+
+end:
     return 0;
+}
+
+/* STATIC FUNCTION DEFINITIONS */
+static void sighandler(int signum)
+{
+    should_shutdown = 1;
 }
